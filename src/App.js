bd88946bb7fe5f4833de8c2a439836071de71a55
klisowska-wlog <86469730+klisowska-wlog@users.noreply.github.com>
2021-07-05 22:42:54 +0200

    Initial comment - added react project and docker setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          NOW <code>src/App.js</code> NOW!!!!!
         </p>
         <a
           className="App-link"
